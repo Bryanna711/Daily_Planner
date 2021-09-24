438845f04334958dee63000adcc2240cfa62cca2
Bryanna Vinge <bevinge711@gmail.com>
2021-09-24 07:35:08 -0500

    possibly finished color change

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
-var d = new Date();
-var n =d.getHours();
-console.log(n);
+// var d = new Date();
+// var n =d.getHours();
+// console.log(n);
 
 var now = moment().format("H")
 
 var hourClass = $(".hour");
-var textArea = $("textarea")
+var textArea = $("textarea");
+
 
 textArea.each(function(){
-    if(now>9){($(this).addClass("past"))}
-    else if (now<9){($(this).addClass("future"))}
+    if(now>9){
+        ($(this).addClass("past"));
+    }
+    else if (now<9){
+        ($(this).addClass("future"));
+    }
+    else if (now === 9){
+        ($("9").addClass("present"));
+    }
+    else if (now ===10){
+        ($("10").addClass("present"));
+    }
+    else if (now === 11){
+        ($("11").addClass("present"));
+    }
+    else if (now === 12){
+        ($("12").addClass("present"));
+    }
+    else if (now === 1){
+        ($("1").addClass("present"));
+    }
+    else if (now === 2){
+        ($("2").addClass("present"));
+    }
+    else if (now === 3){
+        ($("3").addClass("present"));
+    }
+    else if (now === 4){
+        ($("4").addClass("present"));
+    }
+    else if (now === 5){
+        ($("5").addClass("present"));
+    }
 })
 
 
@@ -39,44 +71,44 @@ textArea.each(function(){
 
 
 
-function colorChange(){
-var d = new Date();
-var n = d.getHours();
-if(n >= 9 && n <= 17){
-    $(".text").addClass("present");
-}
-else if(hourClass < 9){
-    $(".text").addClass("past");
-}
-else{
-    $(".text").addClass("future");
-}
-}
+// function colorChange(){
+// var d = new Date();
+// var n = d.getHours();
+// if(n >= 9 && n <= 17){
+//     $(".text").addClass("present");
+// }
+// else if(hourClass < 9){
+//     $(".text").addClass("past");
+// }
+// else{
+//     $(".text").addClass("future");
+// }
+// }
 
-var hour9 = $("#9AM")
-hour9 = 9
+// var hour9 = $("#9AM")
+// hour9 = 9
 
-var hour10 = $("#10AM")
-hour10 = 10
+// var hour10 = $("#10AM")
+// hour10 = 10
 
-var hour11 = $("#11AM")
-hour11 = 11
+// var hour11 = $("#11AM")
+// hour11 = 11
 
-var hour12 = $("#12AM")
-hour12 = 12
+// var hour12 = $("#12AM")
+// hour12 = 12
 
-var hour13 = $("#1PM")
-hour13
+// var hour13 = $("#1PM")
+// hour13
 
-var hour14 = $("#2PM")
-hour14 = 14
+// var hour14 = $("#2PM")
+// hour14 = 14
 
-var hour15 = $("#3PM")
-hour15 = 15
+// var hour15 = $("#3PM")
+// hour15 = 15
 
-var hour16 = $("#4PM")
-hour16 = 16
+// var hour16 = $("#4PM")
+// hour16 = 16
 
-var hour17 = $("#5PM")
-hour17 = 17
+// var hour17 = $("#5PM")
+// hour17 = 17
 
