9c43e63065ff1951feb7a494cd8d7c69f6ff38f3
Bryanna Vinge <bevinge711@gmail.com>
2021-09-24 14:33:29 -0500

    progress on color change - still working on undefined value of key/value pair

--- script.js
+++ script.js
@@ -2,128 +2,83 @@
 // var n =d.getHours();
 // console.log(n);
 
-var now = moment().format("H")
+var today = moment();
+$("#currentDay").text(today.format("MMM Do, YYYY mm:ss"));
+
+var now = moment().format("H");
 
 var hourClass = $(".hour");
 var textArea = $("textarea");
 var saveBtn = $(".saveBtn");
+// 
+// var hour9 = $("9")
+var hour9 = moment(9,"H").isBefore();
+console.log (hour9);
+
+// var hour10 = $("10")
+var hour10 = moment(10,"H").isBefore();
+console.log(hour10);
+
+// var hour11 = $("11")
+var hour11=moment(11,"H").isBefore();
+console.log(hour11)
+
+// var hour12 = $("12")
+var hour12 = moment(12,"H").isBefore();
+console.log(hour12)
+
+// var hour13 = $("1")
+var hour13 = moment(13,"H").isBefore();
+console.log(hour13)
+
+// var hour14 = $("2")
+var hour14 = moment(14,"H").isBefore();
+console.log(hour14)
+
+// var hour15 = $("3")
+var hour15 = moment(15,"H").isBefore();
+console.log(hour15)
+
+// var hour16 = $("4")
+var hour16 = moment(16,"H").isBefore();
+console.log(hour16)
+
+// var hour17 = $("5")
+var hour17 = moment(17,"H").isBefore();
+console.log(hour17)
+
+var hourTime=[hour9, hour10, hour11, hour12, hour13, hour14, hour15, hour16, hour17];
+
+
+textArea.each(function(i) {
+    var time = hourTime[i];
+    var currentTime = $(this).parent().attr("id");
+
+    if(currentTime == now){
+        $(this).toggleclass("present");
+    }
+    else if (time){
+        $(this).toggleClass("past");
+    }   
+    else{
+        $(this).toggleClass("future");
+    }
+    //^^^^present is still not working
+})
 
 
 saveBtn.on("click",function(event){
-    console.log(event.target.value);
-    localStorage.setItem("reminders",textArea.value);
+console.log($(this).siblings('textarea').val());
+;
+  textArea.textContent = textArea.value ;
+   localStorage.setItem("reminders",textArea.value);
     }
-)
-
+ )
+// ^^^^^textArea.value is undefined....????
 textArea.on("input",function (event){
     console.log(event.target.value);
+;
     
 })
 
-
-
-
-textArea.each(function(){
-    if(now>9){
-        ($(this).addClass("past"));
-    }
-    else if (now<9){
-        ($(this).addClass("future"));
-    }
-    // else if (now === 9){
-    //     ($("9").addClass("present"));
-    // }
-    // else if (now ===10){
-    //     ($("10").addClass("present"));
-    // }
-    // else if (now === 11){
-    //     ($("11").addClass("present"));
-    // }
-    // else if (now === 12){
-    //     ($("12").addClass("present"));
-    // }
-    // else if (now === 1){
-    //     ($("1").addClass("present"));
-    // }
-    // else if (now === 2){
-    //     ($("2").addClass("present"));
-    // }
-    // else if (now === 3){
-    //     ($("3").addClass("present"));
-    // }
-    // else if (now === 4){
-    //     ($("4").addClass("present"));
-    // }
-    // else if (now === 5){
-    //     ($("5").addClass("present"));
-    // }
-})
-
-
-// var textArea = $(".textarea");
-
-// var textArray=[];
-// var planner = $("text")
-// planner.value=""
-
-// textArray.push(planner.value)
-
-// console.log(planner.value)
-
-
-// var save =$("saveBtn")
-
-// 
-
-
-
-// function colorChange()
-// {
-//     if(n===hour){
-//       textArea.css("background-color", "red")  
-//     }
-// }
-
-
-
-// function colorChange(){
-// var d = new Date();
-// var n = d.getHours();
-// if(n >= 9 && n <= 17){
-//     $(".text").addClass("present");
-// }
-// else if(hourClass < 9){
-//     $(".text").addClass("past");
-// }
-// else{
-//     $(".text").addClass("future");
-// }
-// }
-
-// var hour9 = $("#9AM")
-// hour9 = 9
-
-// var hour10 = $("#10AM")
-// hour10 = 10
-
-// var hour11 = $("#11AM")
-// hour11 = 11
-
-// var hour12 = $("#12AM")
-// hour12 = 12
-
-// var hour13 = $("#1PM")
-// hour13
-
-// var hour14 = $("#2PM")
-// hour14 = 14
-
-// var hour15 = $("#3PM")
-// hour15 = 15
-
-// var hour16 = $("#4PM")
-// hour16 = 16
-
-// var hour17 = $("#5PM")
-// hour17 = 17
 
