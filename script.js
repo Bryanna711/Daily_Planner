1d5d5368a7675020e3797775491b1dbaadda027b
Bryanna Vinge <bevinge711@gmail.com>
2021-09-23 21:10:28 -0500

    end of night push - working on color change

--- script.js
+++ script.js
@@ -2,62 +2,56 @@ var d = new Date();
 var n =d.getHours();
 console.log(n);
 
-var textArea = $(".textarea");
+var now = moment().format("H")
 
-var textArray=[];
-var planner = $("text")
-planner.value=""
+var hourClass = $(".hour");
+var textArea = $("textarea")
 
-textArray.push(planner.value)
-
-console.log(planner.value)
-
-
-var save =$("saveBtn")
-
-save.on("click",function(){
-
-    console.log(planner.value)
-    // textContent.push(planner);
-    // localStorage.setItem("dailyPlan", json.stringify(textArray));
-
+textArea.each(function(){
+    if(now>9){($(this).addClass("past"))}
+    else if (now<9){($(this).addClass("future"))}
 })
 
 
+// var textArea = $(".textarea");
 
-function colorChange()
-{
-    if(n===hour){
-      textArea.css("background-color", "red")  
-    }
+// var textArray=[];
+// var planner = $("text")
+// planner.value=""
+
+// textArray.push(planner.value)
+
+// console.log(planner.value)
+
+
+// var save =$("saveBtn")
+
+// 
+
+
+
+// function colorChange()
+// {
+//     if(n===hour){
+//       textArea.css("background-color", "red")  
+//     }
+// }
+
+
+
+function colorChange(){
+var d = new Date();
+var n = d.getHours();
+if(n >= 9 && n <= 17){
+    $(".text").addClass("present");
 }
-
-var hour0 = $("#12AM")
-hour0 = 0
-
-var hour1 = $("#1AM")
-hour0 = 1
-
-var hour2 = $("#2AM")
-hour2 =2
-
-var hour3 = $("#3AM")
-hour3 = 3
-
-var hour4 = $("#4AM")
-hour4 = 4
-
-var hour5 = $("#5AM")
-hour5 = 5
-
-var hour6 = $("#6AM")
-hour6 = 6
-
-var hour7 = $("#7AM")
-hour7 = 7
-
-var hour8 = $("#8AM")
-hour8 = 8
+else if(hourClass < 9){
+    $(".text").addClass("past");
+}
+else{
+    $(".text").addClass("future");
+}
+}
 
 var hour9 = $("#9AM")
 hour9 = 9
@@ -85,100 +79,4 @@ hour16 = 16
 
 var hour17 = $("#5PM")
 hour17 = 17
-
-var hour18 = $("#6PM")
-hour18 = 18
-
-var hour19 = $("#7PM")
-hour19 = 19
-
-var hour20 = $("#8PM")
-hour20 = 20
-
-var hour21 = $("#9PM")
-hour21 = 21
-
-var hour22 = $("#10PM")
-hour22 = 22
-
-var hour23 = $("#11PM")
-hour23 = 23
-
-var hour =$(".hour")
-
-var today = moment();
- $("#currentDay").text(today.format("MMM Do, YYYY"));
-
-// var twelveAM = moment("12 AM", "hhA").format("hh A");
-// $("#12AM").text(twelveAM);
-
-// var oneAM = moment("1 AM", "hhA").format("hh A");
-// $("#1AM").text(oneAM);
-
-// var twoAM = moment("2 AM", "hhA").format("hh A");
-// $("#2AM").text(twoAM);
-
-// var threeAM = moment("3 AM", "hhA").format("hh A");
-// $("#3AM").text(threeAM);
-
-// var fourAM = moment("4 AM", "hhA").format("hh A");
-// $("#4AM").text(fourAM);
-
-// var fiveAM = moment("5 AM", "hhA").format("hh A");
-// $("#5AM").text(fiveAM);
-
-// var sixAM = moment("6 AM", "hhA").format("hh A");
-// $("#6AM").text(sixAM);
-
-// var sevenAM = moment("7 AM", "hhA").format("hh A");
-// $("#7AM").text(sevenAM);
-
-// var eightAM = moment("8 AM", "hhA").format("hh A");
-// $("#8AM").text(eightAM);
-
-// var nineAM = moment("9 AM", "hhA").format("hh A");
-// $("#9AM").text(nineAM);
-
-// var tenAM = moment("10 AM", "hhA").format("hh A");
-// $("#10AM").text(tenAM);
-
-// var elevenAM = moment("11 AM", "hhA").format("hh A");
-// $("#11AM").text(elevenAM);
-
-// var twelvePM = moment("12 PM", "hhA").format("hh A");
-// $("#12PM").text(twelvePM);
-
-// var onePM = moment("1 PM", "hhA").format("hh A");
-// $("#1PM").text(onePM);
-
-// var twoPM = moment("2 PM", "hhA").format("hh A");
-// $("#2PM").text(twoPM);
-
-// var threePM = moment("3 PM", "hhA").format("hh A");
-// $("#3PM").text(threePM);
-
-// var fourPM = moment("4 PM", "hhA").format("hh A");
-// $("#4PM").text(fourPM);
-
-// var fivePM = moment("5 PM", "hhA").format("hh A");
-// $("#5PM").text(fivePM);
-
-// var sixPM = moment("6 PM", "hhA").format("hh A");
-// $("#6PM").text(sixPM);
-
-// var sevenPM = moment("7 PM", "hhA").format("hh A");
-// $("#7PM").text(sevenPM);
-
-// var eightPM = moment("8 PM", "hhA").format("hh A");
-// $("#8PM").text(eightPM);
-
-// var ninePM = moment("9 PM", "hhA").format("hh A");
-// $("#9PM").text(ninePM);
-
-// var tenPM = moment("10 PM", "hhA").format("hh A");
-// $("#10PM").text(tenPM);
-
-// var elevenPM = moment("11 PM", "hhA").format("hh A");
-// $("#11PM").text(elevenPM);
-
 
